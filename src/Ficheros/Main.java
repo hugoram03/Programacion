99a85e527b7318be167d020bb3bb77c8c361f25f
hugoram03 <huramsim@gmail.com>
2024-03-14 23:09:52 +0100

    Manejo de la clase File

--- src/Ficheros/Main.java
+++ src/Ficheros/Main.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File fichero1 = new File("fichero.txt");
+        //File fichero1 = new File("fichero.txt");
         File fichero2 = new File("C:\\Users\\adri1\\Programacion\\Tema 6 - POO\\Programacion\\src\\Ficheros\\ficheroPrueba.txt");
-        fichero1.createNewFile();
+        //fichero1.createNewFile();
         fichero2.createNewFile();
 
-        System.out.println("Existe el fichero 1: " + fichero1.exists());
+        //System.out.println("Existe el fichero 1: " + fichero1.exists());
         System.out.println("Existe el fichero 2: " + fichero2.exists());
 
     }
