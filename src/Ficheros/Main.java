f6ccf5eeb96860efd0fefb6d3140c45a44474a1c
Hugo <huramsim@gmail.com>
2024-03-21 13:32:31 +0100

    terminar ejercicios guiados

--- src/Ficheros/Main.java
+++ src/Ficheros/Main.java
@@ -3,16 +3,15 @@ package Ficheros;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
     static Logger LOGGER = LogManager.getRootLogger();
+    static StringBuilder cadena = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        ArrayList<File> ficheros = new ArrayList<>();
-        StringBuilder cadena = new StringBuilder();
+
         File fichero1 = new File("src\\Ficheros\\ficheroprueba2.txt");
         //File fichero2 = new File("C:\\Users\\adri1\\Programacion\\Tema 6 - POO\\Programacion\\src\\Ficheros\\ficheroPrueba.txt");
         File fichero3 = new File("D:\\fichero3.txt");
@@ -20,35 +19,18 @@ public class Main {
         File directorio1 = new File("src\\Ficheros\\directorio1");
         File listaFicheros = new File("src\\Ficheros\\directorio1\\listaFicheros");
         File fichero5 = new File("src\\Ficheros\\ficheroPrueba2.txt");
-        File listaFichero1 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero1.txt");
-        File listaFichero2 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero2.txt");
-        File listaFichero3 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero3.txt");
-        File listaFichero4 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero4.txt");
-        File listaFichero5 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero5.txt");
-        File listaFichero6 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero6.txt");
-        File listaFichero7 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero7.txt");
-        File listaFichero8 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero8.txt");
-        File listaFichero9 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero9.txt");
-        File listaFichero10 = new File("src\\Ficheros\\directorio1\\listaFicheros\\listaFichero10.txt");
+
+
 
         try {
             fichero1.createNewFile();
             //fichero2.createNewFile();
             fichero3.createNewFile();
             fichero5.createNewFile();
-            listaFichero1.createNewFile();
-            listaFichero2.createNewFile();
-            listaFichero3.createNewFile();
-            listaFichero4.createNewFile();
-            listaFichero5.createNewFile();
-            listaFichero6.createNewFile();
-            listaFichero7.createNewFile();
-            listaFichero8.createNewFile();
-            listaFichero9.createNewFile();
-            listaFichero10.createNewFile();
         } catch (IOException e) {
-            LOGGER.error("Error el algun fichero");
+            LOGGER.error("Error en algun fichero");
         }
+
         directorio1.mkdir();
         listaFicheros.mkdir();
 
@@ -74,6 +56,66 @@ public class Main {
         }
         cadena.append("\nficheroPrueba2 creado :" + fichero5.createNewFile());
 
+        cadena.append("\nCreando los 10 ficheros");
+        String ruta = "src\\Ficheros\\directorio1\\listaFicheros";
+        for (int i = 1; i <= 10; i++) {
+            String nombreFichero = ruta + "\\fichero" + i + ".txt";
+            File fichero = new File(nombreFichero);
+            fichero.createNewFile();
+        }
+        cadena.append("\nUso de FileWriter");
+        File creaFichero = new File("src\\Ficheros\\creaFichero.txt");
+        creaFichero.createNewFile();
+        escribeFichero("Ramirez", creaFichero);
+        escribeFichero("Hugo", creaFichero);
+        escribeFichero2("Ramirez", creaFichero);
+        cadena.append("\nUso de FileReader");
+        leeFichero(creaFichero);
+        cadena.append("\nEscribiendo en fichero inexistente");
+        escribeFicheroSinExistencia();
+        usaPrintWriter();
+
+
+
+
         System.out.println(cadena);
+    }
+
+    public static void escribeFichero(String texto, File fichero) throws IOException {
+        FileWriter fileWriter = new FileWriter(fichero);
+        fileWriter.write(texto);
+        fileWriter.close();
+    }
+    public static void escribeFichero2(String texto, File fichero) throws IOException {
+        FileWriter fileWriter = new FileWriter(fichero, true);
+        fileWriter.write(texto);
+        fileWriter.close();
+    }
+    public static void leeFichero(File fichero) throws IOException {
+        FileReader fileReader = new FileReader(fichero);
+        int caracter;
+        cadena.append("\nContenido del fichero" + fichero + ":");
+        while ((caracter = fileReader.read()) != -1) {
+            cadena.append((char) caracter);
+        }
+        fileReader.close();
+    }
+    public static void escribeFicheroSinExistencia() throws IOException {
+        File ficheroNoExiste = new File("src\\Ficheros\\ficheroNoExiste.txt");
+        FileWriter fileWriter = new FileWriter(ficheroNoExiste);
+        fileWriter.write("Hola Mundo");
+        fileWriter.close();
+    }
+    public static void usaPrintWriter() throws IOException {
+        int caracter = 10;
+        File ficheroPrint = new File("src\\Ficheros\\ficheroPrint.txt");
+        PrintWriter printWriter = new PrintWriter(ficheroPrint);
+        printWriter.println("Hola");
+        printWriter.print("mundo");
+        printWriter.format(" dia $d", caracter);
+        printWriter.close();
+    }
+    public static void usaScanner(){
+
     }
 }
